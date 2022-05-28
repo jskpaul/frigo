220f3e6c9d21cf7630279c5d1cc4fc6a11b983de
davidyim03 <66441490+davidyim03@users.noreply.github.com>
2022-05-28 17:03:51 -0500

    env for flask

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,9 @@ rows = cursor.fetchall()
 
 @app.route('/search', methods=['GET'])
 def search():
+    
     args = request.args
-    ingredients = args['ingredient']
+    ingredients = args['ingredients']
     ingredlist = ingredients.split(",")
     ingdict = dict()
     for ing in ingredlist: ingdict[ing] = "Yeeet"
@@ -22,11 +23,12 @@ def search():
 
 
     #cursor.execute("SELECT * FROM recipes WHERE name LIKE '%"+ingredients+"%'")
-    cursor.execute("SELECT * FROM recipes")
+    cursor.execute("SELECT * FROM public.recipes")
     rows = cursor.fetchall()
-
     rowdict = dict()
-    
+    #for count, row in enumerate(rows):
+        #rowdict[count] = row
+    #return rowdict
     for count, row in enumerate(rows):
         
         ings = row[10]
@@ -36,7 +38,6 @@ def search():
         for ing in ings:
             if not ing in ingdict.keys():
                 add = False
-                break
         if add:
             rowdict[count] = row
     return rowdict
