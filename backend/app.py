f44d5666a5353f4a7844eb02036eca3c2dae432c
davidyim03 <66441490+davidyim03@users.noreply.github.com>
2022-05-28 16:08:02 -0500

    line parsing

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
+from ast import arg
 import psycopg2 
 from flask import Flask, jsonify, request
+import sys
 
 app = Flask(__name__)
 
@@ -10,12 +12,31 @@ rows = cursor.fetchall()
 
 @app.route('/search', methods=['GET'])
 def search():
-    
+    args = request.args
+    ingredients = args['ingredient']
+    ingredlist = ingredients.split(",")
+    ingdict = dict()
+    for ing in ingredlist: ingdict[ing] = "Yeeet"
     conn = psycopg2.connect("dbname=recipes user=postgres password=4385")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM recipes WHERE name LIKE '%shrimp%'")
+
+
+    #cursor.execute("SELECT * FROM recipes WHERE name LIKE '%"+ingredients+"%'")
+    cursor.execute("SELECT * FROM recipes")
     rows = cursor.fetchall()
+
     rowdict = dict()
+    
     for count, row in enumerate(rows):
-        rowdict[count] = row
+        
+        ings = row[10]
+        newings = ings[2:-2]
+        ings = newings.split("', '")
+        add = True
+        for ing in ings:
+            if not ing in ingdict.keys():
+                add = False
+                break
+        if add:
+            rowdict[count] = row
     return rowdict
