72cfcd74f8aa3609d51fe391837998c903ba111e
davidyim03 <66441490+davidyim03@users.noreply.github.com>
2022-05-28 19:43:41 -0500

    lil algo fix

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from ast import arg
 import psycopg2 
 from flask import Flask, jsonify, request
 import sys
+import math
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ def search():
     ingredients = args['ingredients']
     ingredlist = ingredients.split(",")
     ingdict = dict()
-    for ing in ingredlist: ingdict[ing] = "Yeeet"
+    for ing in ingredlist: ingdict[ing.replace("%20", " ")] = "Yeeet"
     conn = psycopg2.connect("dbname=recipes user=postgres password=4385")
     cursor = conn.cursor()
 
@@ -34,10 +35,30 @@ def search():
         ings = row[10]
         newings = ings[2:-2]
         ings = newings.split("', '")
-        add = True
+        add = 0
         for ing in ings:
-            if not ing in ingdict.keys():
-                add = False
-        if add:
-            rowdict[count] = row
-    return rowdict
+            if ing in ingdict.keys():
+                add += 1
+        percentage = math.ceil((add/len(ings))*100)
+        if percentage in rowdict.keys():
+            rowdict[percentage].append(row)
+        else:
+            rowdict[percentage] = [row]
+
+    realdict = dict()
+    counter = 0
+    for row in rowdict[100]:
+        realdict[counter]=row
+        counter = counter + 1
+    
+    if counter > 5:
+        return realdict
+     
+    for i in range(1,100):
+        index = 100-i
+        if index in rowdict.keys():
+            for row in rowdict[index]:
+                realdict[counter] = row
+                counter = counter + 1
+                if counter > 5:
+                    return realdict
