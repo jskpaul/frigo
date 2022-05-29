c01fdebfb2cf447fe65d14809417c31d794913ea
Paul <joonsung218@gmail.com>
2022-05-29 03:40:03 -0500

    tags

--- tags/tags.py
+++ tags/tags.py
@@ -14,6 +14,32 @@ rows = cursor.fetchall()
 
 def filter():
     args = request.args
-    tags = args[5]
+    tags = args["tags"]
     taglist = tags.split(",")
+    tagdict = dict()
+    for tag in taglist: tagdict[tag.replace("%20", " ")] = "ugh"
+    result = urlparse("postgresql://bkajobnssdigmz:9b2d5a529654f5da198abae2da464742c6f0525d4c52dc18c7041b5b5a3fd61b@ec2-44-196-223-128.compute-1.amazonaws.com:5432/de246nvqoro4o6")
     
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+    port = result.port
+
+    conn = psycopg2.connect(
+        database = database,
+        user = username,
+        password = password,
+        host = hostname,
+        port = port
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM public.recipes")
+    rows = cursor.fetchall()
+    rowdict = dict()
+    for count, row in enumerate(rows):
+        
+
+
+
